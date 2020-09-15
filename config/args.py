896ef11566c5841b3ce3642693f169369a5c7b0c
DEASFN <71021671+DEASFN@users.noreply.github.com>
2020-09-16 01:02:48 +0800

    Add files via upload

--- config/args.py
+++ config/args.py
@@ -5,11 +5,12 @@ TrainArgs = dotdict({
 	'LoadModel': False,
 	'CheckPoint': './checkpoint/model.pth',
 	'DataPath': './dataset/',
-	'CudaDevice': '3',
-	'subjects': [str(index).zfill(2) for index in range(29)],
-	'actions': ['walk', 'hand', 'run', 'jump'],
-	'GpePaths': ['1-person/01', '1-person/02', '2-person', '3-person', 'multi-person/4-person', 'multi-person/5-person'],
-	'TrainRatio': 0.3,
+	'CudaDevice': '0',
+	'subjects': [str(index).zfill(2) for index in range(30)],
+	'actions': ['walk', 'wave', 'run', 'jump'],
+	'GpePaths': ['1-person/01', '1-person/02', '2-person', '3-person', '4-person', '5-person'],
+	'SpeTrainRatio': 0.3,
+	'GpeTrainRatio': 0.8,
 
 	'MaxEpoch': 30,
 	'LearningRate': 0.001,
@@ -28,18 +29,18 @@ TrainArgs = dotdict({
 TestArgs = dotdict({
 	'CheckPoint': './checkpoint/model.pth',
 	'DataPath': './dataset/',
-	'CudaDevice': '3',
-	'subjects': [str(index).zfill(2) for index in range(29)],
+	'CudaDevice': '0',
+	'subjects': [str(index).zfill(2) for index in range(30)],
 
 	# uniform distribution
 	'SpeInfo': [('walk', [range(1000, 2000, 20)]),
-				('hand', [range(1000, 2000, 20)]),
+				('wave', [range(1000, 2000, 20)]),
 				('run', [range(500, 1000, 10)]),
 				('jump', [range(500, 1000, 10)])],
 
 	# range = (len(data)*0.9-250, len(data)*0.9+250)
-	'GpeInfo': [('multi-person/5-person', range(6400, 6900)),
-				('multi-person/4-person', range(18450, 18950)),
+	'GpeInfo': [('5-person', range(6400, 6900)),
+				('4-person', range(18450, 18950)),
 				('3-person', range(24700, 25200)),
 				('2-person', range(16750, 17250)),
 				('1-person/01', range(5700, 5950)),
