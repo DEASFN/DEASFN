8362da2f1b3f6eff8f5224136daf1d3ff6513311
DEASFN <71021671+DEASFN@users.noreply.github.com>
2020-09-16 00:59:25 +0800

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ pip install numpy
 pip install hdf5storage
 pip install torch==1.4.0+cu92 torchvision==0.5.0+cu92 -f https://download.pytorch.org/whl/torch_stable.html
 pip install tqdm
+pip install scipy
