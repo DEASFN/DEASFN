5db1358800752e4a6eddf272671d036fafbff947
DEASFN <71021671+DEASFN@users.noreply.github.com>
2020-09-17 01:23:30 +0800

    Create download_SPE.sh

---
+++ download_SPE.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd dataset/GPE
+gdown https://drive.google.com/a/nctu.edu.tw/uc?id=18RJokb0RbusMmQCcMapKUV3fWCDZmfe2&export=download
+gdown https://drive.google.com/a/nctu.edu.tw/uc?id=1Ma5sjvuGZJmW6aQd5nC-WF2bLaWhEAPi&export=download
+gdown https://drive.google.com/a/nctu.edu.tw/uc?id=1K2ea7NzFklniONml8EAq4RNQkaxGSpR0&export=download
+gdown https://drive.google.com/a/nctu.edu.tw/uc?id=1x7h2Y5o0Z2X8D9bKR61ZFZA9WZX6V-j7&export=download
+cd ../../
