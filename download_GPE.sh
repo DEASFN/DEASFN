44d087f8a307866b4e31d0b824957d42ad40f568
DEASFN <71021671+DEASFN@users.noreply.github.com>
2020-09-17 01:21:44 +0800

    Create download_GPE.sh

---
+++ download_GPE.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd dataset/GPE
+gdown https://drive.google.com/a/nctu.edu.tw/uc?id=1xHlOrxjZ7cdAdI0YDzHuy2i3QiCFDEl3&export=download
+gdown https://drive.google.com/a/nctu.edu.tw/uc?id=1Z44xXISatL5_Wso18kDlDaRq_b3VcR9I&export=download
+gdown https://drive.google.com/a/nctu.edu.tw/uc?id=16mm8suE5irvzH2UEWIbwg_5MUixhNNzz&export=download
+gdown https://drive.google.com/a/nctu.edu.tw/uc?id=1l23VnkmSMQuMXNb1qbC3F1eku7XM-U3_&export=download
+cd ../../
