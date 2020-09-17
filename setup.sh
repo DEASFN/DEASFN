2441865154c42902328b093adddfb8d445da3076
DEASFN <71021671+DEASFN@users.noreply.github.com>
2020-09-17 14:13:02 +0800

    Delete setup.sh

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-pip install numpy
-pip install hdf5storage
-pip install torch==1.4.0+cu92 torchvision==0.5.0+cu92 -f https://download.pytorch.org/whl/torch_stable.html
-pip install tqdm
-pip install scipy
-pip install gdown
